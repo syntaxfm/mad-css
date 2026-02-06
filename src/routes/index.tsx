95ba3014be73366001b9176a20e13dec03a27fd1
Wes Bos <wesbos@gmail.com>
2026-02-06 16:59:54 -0500

    hide the activity on prod for now

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,11 +12,14 @@ function App() {
 		<div>
 			<Ticket />
 			<div className="section">
-				<div className="section-content" style={{ "--max-width": "1500px", paddingInline: 0 }}>
+				<div
+					className="section-content"
+					style={{ "--max-width": "1500px", paddingInline: 0 }}
+				>
 					<Roster />
 				</div>
 			</div>
-			<Activity />
+			{/* <Activity /> */}
 			<div className="section" style={{ paddingInline: 0 }}>
 				<h2>The Bracket</h2>
 				<Bracket />
